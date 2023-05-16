18db5cbe52f395336a6e0e5b6557321fa994b9d2
kvelez77 <kvelezmendoza71@gmail.com>
2023-05-16 11:36:04 -0500

    primer

---
+++ calculadora/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese cuántos números desea utilizar: ");
+        string input = Console.ReadLine();
+        int count;
+
+        
